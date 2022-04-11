645a33f0a4721e7fa675c478b15b645cbf958cc7
ReitoSerizawa <s.reitiger@gmail.com>
2022-04-11 14:59:52 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector ('form')
+  form.addEventListener ('submit', function (e) {
+    e.preventDefault()
+    createList(document.getElementById('new-task-description').value)
+  })
+
+  function createList (typedToDo) {
+  let createLiTags = document.createElement('li')
+  createLiTags.textContent = typedToDo
+  createLiTags.className = 'list_of_todos'
+ 
+
+  document.getElementById('tasks').appendChild(createLiTags)
+
+  
+  let createBttn = document.createElement('button')
+  createBttn.textContent = 'Delete'
+
+  createLiTags.append(createBttn)
+    
+  createBttn.addEventListener ('click', () => {
+    createLiTags.remove()
+  })
+
+
+
+}
+
+
+
 });
+
+
